5c91531b1e4bb9c1b6b8f52a1231e069c9b98af9
Tim Moorhouse <timmoorhouse@gmail.com>
2014-07-24 09:58:50 -0400

    add nfs.showmount api

--- usr/lib/net/nfs.lua
+++ usr/lib/net/nfs.lua
@@ -1,8 +1,15 @@
 
+local net = require("net")
+
 local nfs = {
 	protocol = "nfs",
 }
 
 autoload(nfs, "net.nfs", "Mount", "Server")
+
+function nfs.showmount(srv, dir, timeout)
+	local host = net.dns.resolve(net.nfs.protocol, srv)
+	return net.rpc.rpc(net.nfs.protocol, host, { timeout=timeout, }, "showmounts", dir)
+end
 
 return nfs
